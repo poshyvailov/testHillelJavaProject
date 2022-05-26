4003a3896629b476015333050d180b04507a8ba5
poshyvailov <pelmel12@gmail.com>
2022-05-26 10:52:05 +0300

    Added switch

--- src/hillel/test/Lesson2.java
+++ src/hillel/test/Lesson2.java
@@ -32,6 +32,80 @@ public class Lesson2 {
 //            System.out.println("Wrong input data");
 //        }
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type your mark to convert it ");
+        int userMark = scanner.nextInt();
+
+        if (userMark <= 0 || userMark > 12) {
+            System.out.println("Use values from 1 to 12! ");
+        } else
+            switch (userMark) {
+                case 12: {
+                    userMark = 5;
+                    System.out.println("Congratulation! Your mark is " + userMark);
+                    break;
+                }
+                case 11: {
+                    userMark = 5;
+                    System.out.println("Congratulation! Your mark is " + userMark);
+                    break;
+                }
+                case 10: {
+                    userMark = 5;
+                    System.out.println("Congratulation! Your mark is " + userMark);
+                    break;
+                }
+                case 9: {
+                    userMark = 4;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 8: {
+                    userMark = 4;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 7: {
+                    userMark = 4;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 6: {
+                    userMark = 3;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 5: {
+                    userMark = 3;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 4: {
+                    userMark = 3;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 3: {
+                    userMark = 2;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 2: {
+                    userMark = 2;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                case 1: {
+                    userMark = 2;
+                    System.out.println("Your mark is " + userMark);
+                    break;
+                }
+                default: {
+                    System.out.println("Wrong value! ");
+                }
+            }
+
     }
 
 
