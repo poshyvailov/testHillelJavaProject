ee38b167e66656c35735eb1a1870efa52835d9e8
poshyvailov <pelmel12@gmail.com>
2022-05-30 14:14:12 +0300

    Lesson 3 Testing for with factorial, preparing random number, converting random number to int

--- src/hillel/test/Lesson3.java
+++ src/hillel/test/Lesson3.java
@@ -38,21 +38,70 @@ public class Lesson3 {
 //        }
 
 
-        //while
-        int userId = 0;
-        while (userId <= 100) {
-            System.out.println("Your user is is: " + userId);
-            userId++;
+//        //while
+//        int userId = 0;
+//        while (userId <= 100) {
+//            System.out.println("Your user is is: " + userId);
+//            userId++;
+//        }
+//
+//
+//
+//        //do while
+//        Scanner scan = new Scanner(System.in);
+//        int count = 0;
+//
+//        do {
+//            System.out.println("Your count is: " + count);
+//            count++;
+//        } while (count < 100);
+
+
+
+//        //for
+//        for (int a = 0; a <= 10; a++){
+//            System.out.println("int a = " + a);
+//        }
+//        System.out.println("For is finished");
+
+
+        //Факториал 5
+        // 5! = 1 * 2 * 3 * 4 * 5 = 120
+        // Факториал не может быть с 0
+        // Факторил не может быть негативным
+
+
+
+        //Will generate random number from 0 to 1 DOUBLE
+        System.out.println(Math.random());
+        //Если мы хотим его преобразать к целому
+        System.out.println(Math.random() * 10);
+
+        //приобразовуем в целое int
+        int randomNumber = (int) (Math.random() * 10);
+        System.out.println(randomNumber);
+
+
+//        int factorial = 5;
+//        int fResult = 1;
+//
+//        for (int i = 1; i <= factorial; i++){
+//            fResult  *= i;
+//            System.out.println("Current result is: " + fResult);
+//        }
+//        System.out.println("Final factorial result is: " + fResult);
+
+        
+        //Усложняем вычисление факториала рандомным числом
+        int factorial = (int)(Math.random() * 10);
+        System.out.println(factorial);
+        int fResult2 = 1;
+
+        for (int i = 1; i <= factorial; i++){
+            fResult2 *= i;
+            System.out.println("Current factorial is: " + fResult2);
         }
-
-        //do while
-        Scanner scan = new Scanner(System.in);
-        int count = 0;
-
-        do {
-            System.out.println("Your count is: " + count);
-            count++;
-        } while (count < 100);
+        System.out.println("Final factorial is: " + fResult2);
 
 
     }
